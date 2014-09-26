ea55912f77e072d2e15b257ef7beb10eb6b2331f
Pablo Marconi <pablo.marconi.ar@gmail.com>
2014-09-26 17:48:04 -0300

    Limpieza de codigo en la declaracion de metodo de Cup

--- src/FunctionDescriptor.java
+++ src/FunctionDescriptor.java
@@ -4,8 +4,23 @@ import java.util.*;
 
 public class FunctionDescriptor extends Descriptor {
 
+	private LinkedList<VarDescriptor> parameters;
+
 	public FunctionDescriptor(Type newType, String newName){
 		super(newType, newName);
+	}
+
+	public FunctionDescriptor(Type newType, String newName, LinkedList<VarDescriptor> params){
+		super(newType, newName);
+		parameters = params;
+	}
+
+	public LinkedList<VarDescriptor> getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(LinkedList<VarDescriptor> params) {
+		parameters = params;
 	}
 
 }
