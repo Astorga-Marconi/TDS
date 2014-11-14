ce25c0b0ed89b9d5575ab1d15bf79479168d1c9e
Dario Astorga <astorgadm@gmail.com>
2014-11-14 19:00:41 -0300

    Agrego encabezado.

--- src/semanticAnalyzer/VarDescriptor.java
+++ src/semanticAnalyzer/VarDescriptor.java
@@ -1,11 +1,19 @@
+/**
+ * Copyright (C) 2014
+ * All rights reserved.
+ *
+ * File Name: VarDescriptor.java
+ * Authors: Astorga Dario - Marconi Pablo
+ * Version: 0.1 
+ */
 package semanticAnalyzer;
 
 import ir.ast.*;
 
 public class VarDescriptor extends Descriptor {
 
-		public VarDescriptor (Type newType, String newName){
-			super(newType, newName);
-		}
+	public VarDescriptor (Type newType, String newName){
+		super(newType, newName);
+	}
 		
 }
